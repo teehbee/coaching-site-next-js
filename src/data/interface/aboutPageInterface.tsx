7c8878d5fb709818dabdd7d64af6fa582ca081b4
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2025-06-19 13:21:12 +0200

    create interface for content on about page

---
+++ src/data/interface/aboutPageInterface.tsx
@@ -0,0 +1,62 @@
+import { PortableTextBlock } from "@portabletext/react";
+
+export interface AboutPageInterface {
+  aboutTopSection: {
+    aboutTopSectionTitle: string;
+    aboutTopSectionText: PortableTextBlock;
+    aboutTopSectionLinkText: string;
+    aboutTopSectionLinkDestination: string;
+    aboutTopSectionImage: {
+      asset: {
+        _id: string;
+        url: string;
+      };
+      alt: string;
+    };
+  };
+  aboutPageImageAndTextTile: {
+    aboutPageImageAndTextTileTitle: string;
+    aboutPageImageAndTextTileTextContent: PortableTextBlock;
+    aboutPageImageAndTextTileImage: {
+      asset: {
+        _id: string;
+        url: string;
+      };
+      alt: string;
+    };
+  };
+  aboutPageTextBoxesSection: {
+    aboutPageTextBoxesHeadeding: string;
+    aboutPageTextBoxes: {
+      frontpageAlternatingTilesText: PortableTextBlock;
+    }[];
+  };
+  aboutPageLowerBanner: {
+    aboutPageLowerBannerTitle: string;
+    aboutPageLowerBannerTextContent: PortableTextBlock;
+    aboutPageBottomBannerImage: {
+      asset: {
+        _id: string;
+        url: string;
+      };
+      alt: string;
+    };
+  };
+  aboutPageResumeSection: {
+    aboutPageResumeSectionHeading: string;
+    aboutPageResumeSectionList: {
+      aboutPageResumeSectionListItemFromYear: string;
+      aboutPageResumeSectionListItemToYear: string;
+      aboutPageResumeSectionListItemCompany: string;
+      aboutPageResumeSectionListItemPosition: string;
+      aboutPageResumeSectionListItemDescription: PortableTextBlock;
+    }[];
+  };
+  reviewsSection?: {
+    _id: string;
+    ReviewSection: {
+      reviewSectionText: string;
+      reviewSectionName: string;
+    }[];
+  };
+}
