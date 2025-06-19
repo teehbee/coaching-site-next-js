62844a575131c3cd4c5aec28d6f3f0e7fea61eda
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2025-06-19 10:20:48 +0200

    edit hook for fetching sanity data

--- src/utils/useSanityQuery.tsx
+++ src/utils/useSanityQuery.tsx
@@ -3,33 +3,15 @@
 import { useState, useEffect } from "react";
 import { client } from "@/lib/sanityClient";
 
-export function useSanityQuery<T>(query: string, params: Record<string, any> = {}) {
+function useSanityData<T>(query: string, params?: Record<string, any>) {
   const [data, setData] = useState<T | null>(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<Error | null>(null);
 
   useEffect(() => {
-    let isMounted = true;
+    client.fetch(query, params).then((res) => setData(res));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [query, JSON.stringify(params)]);
 
-    client
-      .fetch(query, params)
-      .then((res: T) => {
-        if (isMounted) {
-          setData(res);
-          setLoading(false);
-        }
-      })
-      .catch((err: Error) => {
-        if (isMounted) {
-          setError(err);
-          setLoading(false);
-        }
-      });
-
-    return () => {
-      isMounted = false;
-    };
-  }, [query, params]);
-
-  return { data, loading, error };
+  return data;
 }
+
+export default useSanityData;
