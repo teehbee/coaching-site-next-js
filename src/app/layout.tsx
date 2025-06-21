e720939560aed54da4a1ba843e170cad07770549
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2025-06-21 17:43:19 +0200

    minor corrections to ga scripts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,16 +40,20 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`} strategy="afterInteractive" />
-        <Script id="gtag-init" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
-              page_path: window.location.pathname,
-            });
-          `}
-        </Script>
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+        page_path: window.location.pathname,
+      });
+    `,
+          }}
+        />
       </head>
       <body className={`${playfairDisplay.variable} ${openSans.variable}`}>
         <GoogleAnalytics /> <Header />
