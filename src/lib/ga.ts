55a6a7cc87997e9052565e6f571a6dd87099345b
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2025-06-21 16:59:20 +0200

    create separate function for google analytic to reuse in other projects

---
+++ src/lib/ga.ts
@@ -0,0 +1,21 @@
+export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID || "";
+
+// Sender en sidevisning til GA
+export const pageview = (url: string) => {
+  if (!GA_TRACKING_ID) return;
+
+  window.gtag("config", GA_TRACKING_ID, {
+    page_path: url,
+  });
+};
+
+// Send event (valgfritt)
+export const event = ({ action, category, label, value }: { action: string; category: string; label?: string; value?: number }) => {
+  if (!GA_TRACKING_ID) return;
+
+  window.gtag("event", action, {
+    event_category: category,
+    event_label: label,
+    value: value,
+  });
+};
