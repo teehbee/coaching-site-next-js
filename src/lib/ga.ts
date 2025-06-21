7efc018680a23428c0af9120ffb1b06c14c401ae
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2025-06-21 21:42:14 +0200

    debugging

--- src/lib/ga.ts
+++ src/lib/ga.ts
@@ -1,23 +1,11 @@
 export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID || "";
 
-// Page view
+// Page view (bruker 'event' istedenfor 'config' etter første last)
 export const pageview = (url: string) => {
   if (!GA_TRACKING_ID) return;
-  if (typeof window.gtag !== "function") {
-    return;
-  }
-  window.gtag("config", GA_TRACKING_ID, {
+  if (typeof window.gtag !== "function") return;
+
+  window.gtag("event", "page_view", {
     page_path: url,
   });
 };
-
-// Event logging
-// export const event = ({ action, category, label, value }: { action: string; category: string; label?: string; value?: number }) => {
-//   if (!GA_TRACKING_ID) return;
-
-//   window.gtag("event", action, {
-//     event_category: category,
-//     event_label: label,
-//     value: value,
-//   });
-// };
