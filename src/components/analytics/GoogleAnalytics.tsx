a6ed4ec731dd79ff46e6badd72b95375a0c5448b
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2025-06-21 20:56:12 +0200

    change in google analytics script

--- src/components/analytics/GoogleAnalytics.tsx
+++ src/components/analytics/GoogleAnalytics.tsx
@@ -1,30 +1,48 @@
+// "use client";
+
+// import { useEffect, useRef } from "react";
+// import { usePathname } from "next/navigation";
+// import * as ga from "@/lib/ga";
+
+// export function GoogleAnalytics() {
+//   const pathname = usePathname();
+//   const previousPath = useRef<string>("");
+
+//   // useEffect(() => {
+//   //   const handle = requestAnimationFrame(() => {
+//   //     if (pathname !== previousPath.current) {
+//   //       previousPath.current = pathname;
+//   //       ga.pageview(pathname);
+//   //     }
+//   //   });
+
+//   //   return () => cancelAnimationFrame(handle);
+//   // }, [pathname]);
+
+//   useEffect(() => {
+//     if (pathname !== previousPath.current) {
+//       previousPath.current = pathname;
+//       ga.pageview(pathname);
+//     }
+//   }, [pathname]);
+
+//   return null;
+// }
+
 "use client";
 
-import { useEffect, useRef } from "react";
-import { usePathname } from "next/navigation";
+import { useEffect } from "react";
+import { usePathname, useSearchParams } from "next/navigation";
 import * as ga from "@/lib/ga";
 
 export function GoogleAnalytics() {
   const pathname = usePathname();
-  const previousPath = useRef<string>("");
-
-  // useEffect(() => {
-  //   const handle = requestAnimationFrame(() => {
-  //     if (pathname !== previousPath.current) {
-  //       previousPath.current = pathname;
-  //       ga.pageview(pathname);
-  //     }
-  //   });
-
-  //   return () => cancelAnimationFrame(handle);
-  // }, [pathname]);
+  const searchParams = useSearchParams();
 
   useEffect(() => {
-    if (pathname !== previousPath.current) {
-      previousPath.current = pathname;
-      ga.pageview(pathname);
-    }
-  }, [pathname]);
+    const url = pathname + (searchParams?.toString() ? `?${searchParams.toString()}` : "");
+    ga.pageview(url);
+  }, [pathname, searchParams]);
 
   return null;
 }
