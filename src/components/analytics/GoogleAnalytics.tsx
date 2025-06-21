f48e75d2b957ce7fcf70e00c5b91be982ecab1f9
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2025-06-21 20:33:19 +0200

    add more robust debouce to google analytics script to make sure ga registers page changes

--- src/components/analytics/GoogleAnalytics.tsx
+++ src/components/analytics/GoogleAnalytics.tsx
@@ -1,20 +1,22 @@
 "use client";
 
-// Google analytics script for page views with a simple debouce to avoid double page reports
-
 import { useEffect, useRef } from "react";
 import { usePathname } from "next/navigation";
 import * as ga from "@/lib/ga";
 
 export function GoogleAnalytics() {
   const pathname = usePathname();
-  const previousPath = useRef<string | null>(null);
+  const previousPath = useRef<string>("");
 
   useEffect(() => {
-    if (pathname !== previousPath.current) {
-      previousPath.current = pathname;
-      ga.pageview(pathname);
-    }
+    const handle = requestAnimationFrame(() => {
+      if (pathname !== previousPath.current) {
+        previousPath.current = pathname;
+        ga.pageview(pathname);
+      }
+    });
+
+    return () => cancelAnimationFrame(handle);
   }, [pathname]);
 
   return null;
