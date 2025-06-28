990d9749549b5811135fc8b48145f4d02d007237
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2025-06-28 16:10:14 +0200

    remove usercentrics and google analytics script to change with tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from "next";
-import Script from "next/script";
 import "../styles/styles.scss";
 import { Header, Footer } from "@/components/navigation";
 import { getFontClasses } from "@/lib/fonts";
-import GoogleAnalyticsConsent from "@/analytics/GoogleAnalyticsConsent";
 
 export const metadata: Metadata = {
   title: {
@@ -33,9 +31,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={getFontClasses()}>
-        <Script src="https://web.cmp.usercentrics.eu/modules/autoblocker.js" strategy="beforeInteractive" />
-        <Script id="usercentrics-cmp" src="https://web.cmp.usercentrics.eu/ui/loader.js" data-settings-id="CWoF1AuWdmx_BX" async strategy="afterInteractive" />
-        <GoogleAnalyticsConsent />
         <Header />
         {children}
         <Footer />
