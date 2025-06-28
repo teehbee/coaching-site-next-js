7411604c3381788fed174fb28b4caccc03e82926
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2025-06-28 19:10:16 +0200

    load cmp in next

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import "../styles/styles.scss";
 import { Header, Footer } from "@/components/navigation";
-import Script from "next/script";
 import { getFontClasses } from "@/lib/fonts";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: {
@@ -24,38 +24,18 @@ export const viewport = {
   initialScale: 1,
 };
 
-const gtmId = process.env.NEXT_PUBLIC_GTM_ID;
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const isGtmDefined = typeof gtmId === "string" && gtmId.trim().length > 0;
-
-  if (!isGtmDefined && process.env.NODE_ENV !== "production") {
-    console.warn(" GTM-ID mangler. Sjekk .env-filen din for NEXT_PUBLIC_GTM_ID");
-  }
   return (
     <html lang="sv">
       <head>
-        {/* GTM init script */}
-        <Script id="gtm-init" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','${gtmId}');
-          `}
-        </Script>
+        <Script src="https://web.cmp.usercentrics.eu/modules/autoblocker.js" strategy="beforeInteractive" />
+        <Script id="usercentrics-cmp" src="https://web.cmp.usercentrics.eu/ui/loader.js" data-settings-id="CWoF1AuWdmx_BX" async strategy="beforeInteractive" />
       </head>
       <body className={getFontClasses()}>
-        {/* GTM noscript (fallback) */}
-        <noscript>
-          <iframe src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`} height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe>
-        </noscript>
-
         <Header />
         {children}
         <Footer />
