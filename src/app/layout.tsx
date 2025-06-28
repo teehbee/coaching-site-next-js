fd8fa3cbed8c62e8d05071578779b0c40b9251a4
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2025-06-28 20:49:05 +0200

    fix typo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -58,7 +58,7 @@ export default function RootLayout({
         <Script src="https://web.cmp.usercentrics.eu/modules/autoblocker.js" strategy="beforeInteractive" />
         <Script id="usercentrics-cmp" src="https://web.cmp.usercentrics.eu/ui/loader.js" data-settings-id="CWoF1AuWdmx_BX" async />
         <noscript>
-          <iframe src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`} height="0" width="0" style={{ display: "none", visibility: "hidden" }} />
+          <iframe src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`} height="0" width="0" style={{ display: "none", visibility: "hidden" }} />
         </noscript>
 
         <Header />
