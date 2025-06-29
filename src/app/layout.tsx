7a924bc30c31c41eaa4117aaa0304cbdca29d7af
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2025-06-29 10:53:21 +0200

    remove usercentrics and implement cookiebot

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "../styles/styles.scss";
 import { Header, Footer } from "@/components/navigation";
 import { getFontClasses } from "@/lib/fonts";
-import Script from "next/script";
+// import Script from "next/script";
 
 export const metadata: Metadata = {
   title: {
@@ -24,7 +24,7 @@ export const viewport = {
   initialScale: 1,
 };
 
-const gtmId = process.env.NEXT_PUBLIC_GTM_ID;
+// const gtmId = process.env.NEXT_PUBLIC_GTM_ID;
 
 export default function RootLayout({
   children,
@@ -33,34 +33,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="sv">
-      <head>
-        {/* Google Tag Manager */}
-        <Script
-          id="gtm-script"
-          strategy="beforeInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){
-                w[l]=w[l]||[];
-                w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
-                var f=d.getElementsByTagName(s)[0],
-                    j=d.createElement(s),
-                    dl=l!='dataLayer'?'&l='+l:'';
-                j.async=true;
-                j.src='https://www.googletagmanager.com/gtm.js?id=${gtmId}'+dl;
-                f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','${gtmId}');
-            `,
-          }}
-        />
-      </head>
       <body className={getFontClasses()}>
-        <Script src="https://web.cmp.usercentrics.eu/modules/autoblocker.js" strategy="beforeInteractive" />
-        <Script id="usercentrics-cmp" src="https://web.cmp.usercentrics.eu/ui/loader.js" data-settings-id="CWoF1AuWdmx_BX" async />
-        <noscript>
-          <iframe src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`} height="0" width="0" style={{ display: "none", visibility: "hidden" }} />
-        </noscript>
-
         <Header />
         {children}
         <Footer />
