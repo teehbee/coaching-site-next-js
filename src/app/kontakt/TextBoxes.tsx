4161289ba60dd42772d3e40b4259b2937a6657d6
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2025-07-02 19:41:16 +0200

    cosmetic changes to text boxes on contact page

--- src/app/kontakt/TextBoxes.tsx
+++ src/app/kontakt/TextBoxes.tsx
@@ -17,8 +17,8 @@ const TextBoxes: React.FC = () => {
         <div className="row">
           {settings.contactPageTextBoxes.map((box, index) => (
             <div key={index} className="col-12 col-md-6 col-lg-3 mb-30 d-flex mh-150px">
-              <div className="border-full border-radius-5 text-center p-15 w-100">
-                <div className="pb-15">{box.contactPageTextBoxHeading}</div>
+              <div className="border-full border-radius-5 text-center p-15 pt-30 w-100 d-flex flex-column">
+                <div className="pb-15 fw-700 mh-lg-80px fs-1-rem-lg-1-25rem">{box.contactPageTextBoxHeading}</div>
                 <div className="pb-15">
                   <PortableText value={box.contactPageTextBoxTextContent} />
                 </div>
